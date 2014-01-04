6985f0f70440f7dc1272143ff6cfbcd048bfc3f9
Darron Froese <darron@froese.org>
2014-01-04 15:53:35 -0700

    First commit.

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+# encoding: utf-8
+name             'ubuntu-base'
+maintainer       'Darron Froese'
+maintainer_email 'darron@froese.org'
+license          'Apache 2.0'
+description      'Installs/configures something'
+version          '0.1.0'
+recipe           'ubuntu-base::default', 'Installs/configures something'
